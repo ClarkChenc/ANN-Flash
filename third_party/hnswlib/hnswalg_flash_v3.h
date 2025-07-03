05c4e5ea16a7ba80cd00628f418d13d1370a7227
clarkchenc <clarkchenc@qq.com>
2025-07-03 14:15:14 +0800

    up

--- third_party/hnswlib/hnswalg_flash_v3.h
+++ third_party/hnswlib/hnswalg_flash_v3.h
@@ -1774,12 +1774,16 @@ class HierarchicalNSWFlash_V3 {
         // res[i] = tmp_ret1 + tmp_ret2;
 
         pq_dist_t tmp = 0;
-        for (int j = 0; j < subspace_num_; j += 2) {
+        for (int j = 0; j < subspace_num_; j += 8) {
           tmp += lookup_ptr[pVect2[j]];
           tmp += lookup_ptr[cluster_num_ + pVect2[j + 1]];
-          // tmp += lookup_ptr[2 * cluster_num_ + pVect2[j + 2]];
-          // tmp += lookup_ptr[3 * cluster_num_ + pVect2[j + 3]];
-          lookup_ptr += 2 * cluster_num_;
+          tmp += lookup_ptr[2 * cluster_num_ + pVect2[j + 2]];
+          tmp += lookup_ptr[3 * cluster_num_ + pVect2[j + 3]];
+          tmp += lookup_ptr[4 * cluster_num_ + pVect2[j + 4]];
+          tmp += lookup_ptr[5 * cluster_num_ + pVect2[j + 5]];
+          tmp += lookup_ptr[6 * cluster_num_ + pVect2[j + 6]];
+          tmp += lookup_ptr[7 * cluster_num_ + pVect2[j + 7]];
+          lookup_ptr += 8 * cluster_num_;
         }
         pVect2 += subspace_num_;
         res[i] = tmp;
