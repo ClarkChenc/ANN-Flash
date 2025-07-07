4a83199eb32f46d34d481041aaa5f42c827236cf
clarkchenc <clarkchenc@qq.com>
2025-07-07 22:10:01 +0800

    fix flash dist

--- third_party/hnswlib/hnswalg_flash_v4.h
+++ third_party/hnswlib/hnswalg_flash_v4.h
@@ -1995,15 +1995,16 @@ class HierarchicalNSWFlash_V4 : public AlgorithmInterface<dist_t> {
     dist_t* ptr_vec1 = (dist_t*)p_vec1;
     encode_t* ptr_vec2 = (encode_t*)p_vec2;
 
+    /*
     for (size_t i = 0; i < subvec_num_v4_; ++i) {
       ret += ptr_vec1[*ptr_vec2];
       ptr_vec1 += CLUSTER_NUM;
       ptr_vec2 += 1;
     }
+    */
 
     // Unroll 4 times
-    size_t i = 0;
-    for (; i + 3 < subvec_num_v4_; i += 4) {
+    for (size_t i = 0; i < subvec_num_v4_; i += 4) {
       ret += ptr_vec1[ptr_vec2[0]];
       ret += ptr_vec1[CLUSTER_NUM + ptr_vec2[1]];
       ret += ptr_vec1[2 * CLUSTER_NUM + ptr_vec2[2]];
@@ -2011,13 +2012,6 @@ class HierarchicalNSWFlash_V4 : public AlgorithmInterface<dist_t> {
 
       ptr_vec1 += 4 * CLUSTER_NUM;
       ptr_vec2 += 4;
-    }
-
-    // Tail
-    for (; i < subvec_num_v4_; ++i) {
-      ret += ptr_vec1[*ptr_vec2];
-      ptr_vec1 += CLUSTER_NUM;
-      ptr_vec2 += 1;
     }
 
     return ret;
