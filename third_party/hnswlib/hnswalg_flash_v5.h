f934a6f6ee5aeacbe853545de5bc92331373dc21
clarkchenc <clarkchenc@qq.com>
2025-07-17 11:02:06 +0800

    fix v5 add adapt train

--- third_party/hnswlib/hnswalg_flash_v5.h
+++ third_party/hnswlib/hnswalg_flash_v5.h
@@ -1391,12 +1391,9 @@ class HnswFlash {
             ptr_tmp_table += 1;
             continue;
           }
-          Eigen::VectorXf v1 =
-              Eigen::Map<Eigen::VectorXf>(cur_codebook_ptr + c1 * subspace_len, subspace_len);
-          Eigen::VectorXf v2 =
-              Eigen::Map<Eigen::VectorXf>(cur_codebook_ptr + c2 * subspace_len, subspace_len);
 
-          *ptr_tmp_table = rerank_func_(v1.data(), v2.data(), &subspace_len);
+          *ptr_tmp_table = rerank_func_(cur_codebook_ptr + c1 * subspace_len,
+                                        cur_codebook_ptr + c2 * subspace_len, &subspace_len);
           pq_min_ = std::min(pq_min_, *ptr_tmp_table);
           max_dis = std::max(max_dis, *ptr_tmp_table);
           ptr_tmp_table += 1;
